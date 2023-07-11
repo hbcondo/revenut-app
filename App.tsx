0c7184a182189a290f9fe1b87e5c2ffbcb219dcb
Amar Kota <amar@amarkota.com>
2023-07-11 14:51:50 -0700

    init commit for enabling web service workers
    https://github.com/expo/fyi/blob/main/enabling-web-service-workers.md

--- App.tsx
+++ App.tsx
@@ -2,6 +2,9 @@ import { NativeBaseProvider } from 'native-base';
 import { RevenutDashboard } from './components/Dashboard';
 import * as WebBrowser from 'expo-web-browser';
 
+import * as serviceWorkerRegistration from "./src/serviceWorkerRegistration";
+
+/* https://docs.expo.dev/guides/authentication/ */
 WebBrowser.maybeCompleteAuthSession();
 
 export default function App() {
@@ -11,3 +14,8 @@ export default function App() {
 		</NativeBaseProvider>
 	);
 }
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
